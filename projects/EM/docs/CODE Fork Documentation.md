9a5999aab9d84809d69e6c4e172e4458a1804e9b
Jesse Abdul <jesse.abdul@noaa.gov>
2026-07-31 13:41:23 -1000

    configuring project to implement CODE framework and to deploy the EM data model after the ORDS installation has completed

---
+++ projects/EM/docs/CODE Fork Documentation.md
@@ -0,0 +1,73 @@
+# PIFSC EM Containerized Oracle Developer Environment
+
+## Overview
+The PIFSC Containerized Oracle Developer Environment (CODE) project was developed to provide a containerized Oracle development environment for PIFSC software developers. The Electronic Monitoring (EM) was forked from the CODE project or a downstream forked CODE project to extend the CODE project's functionality and integrate EM's dependencies. This repository can be forked to extend the existing functionality to any data systems that depend on the EM for both development and testing purposes.
+
+## Resources
+-   ### Version Control Information
+    -   URL: git@github.com:noaa-pifsc/PIFSC-EM-Containerized-Oracle-Development-Environment.git
+    -   Version: 1.0 (git tag: EM_CODE_v1.0)
+    -   Upstream Repositories (in order from direct parent to top-level parent):
+        -   CODE Version Control Information:
+            -   URL: git@github.com:noaa-pifsc/PIFSC-Containerized-Oracle-Development-Environment.git
+            -   Version: 1.4 (git tag: CODE_v1.4)
+
+## Intended Use
+-   Refer to the CODE [Intended Use](../../../../core/docs/CODE%20Documentation.md#intended-use) documentation for details
+
+## Requirements
+-   Refer to the CODE [Requirements](../../../../core/docs/CODE%20Documentation.md#requirements) documentation for details
+
+## Container Host Instances
+-   Refer to the CODE [Container Host Instances](../../../../core/docs/CODE%20Documentation.md#container-host-instances) documentation for details
+
+## Dependencies
+-   Refer to the CODE [Dependencies](../../../../core/docs/CODE%20Documentation.md#dependencies) for details
+-   Custom Dependencies:
+    -   ### EM Version Control Information
+        -   folder path: [/projects/EM/modules/EM](../../EM/modules/EM) 
+        -   Version Control Information:
+            -   URL: <git@github.com:WPacFIN/em-database.git>
+            -   Version 0.1 (git tag: em_db_v0.1)
+
+## Container Architecture
+-   Refer to the CODE [Container Architecture](../../../../core/docs/CODE%20Documentation.md#container-architecture) documentation for details
+
+## Naming Conventions
+-   Refer to the CODE [Repository Fork Diagram](../../../../core/docs/CODE%20Documentation.md#naming-conventions) documentation for details
+
+## Repository Fork Diagram
+-   Refer to the CODE [Repository Fork Diagram](../../../../core/docs/CODE%20Documentation.md#repository-fork-diagram) documentation for details
+
+# CODE Folder Structure
+-   Refer to the CODE [Folder Structure](../../../../core/docs/CODE%20Documentation.md#code-folder-structure) documentation for details
+
+## CODE Business Rules
+-   Refer to the CODE [Business Rules](../../../../core/docs/CODE%20Documentation.md#code-business-rules) documentation for details
+
+## CODE Implementation Procedure
+-   Refer to the CODE [Implementation Procedure](../../../../core/docs/CODE%20Documentation.md#code-implementation-procedure) documentation for details
+
+## Setup
+-   Refer to the CODE [Setup](../../../../core/docs/CODE%20Documentation.md#setup) documentation for details
+
+## Executing the CODE Project
+-   Refer to the [Executing the CODE Project](../../../../core/docs/CODE%20Documentation.md#executing-the-code-project) documentation for details
+
+## Contribution and Repository Management Guidelines
+-   Refer to the [Contribution and Repository Management Guidelines](../../../../core/docs/CODE%20Documentation.md#contribution-and-repository-management-guidelines) documentation for details
+
+## Monitoring and Syncing Upstream Updates
+-   Refer to the [Monitoring and Syncing Upstream Updates](../../../../core/docs/CODE%20Documentation.md#monitoring-and-syncing-upstream-updates) documentation for details
+
+## Connection Information
+-   Refer to the CODE [Connection Information](../../../../core/docs/CODE%20Documentation.md#connection-information) documentation for details
+    -   The individual account passwords can be found in the /secrets/secrets.sh file 
+
+## Security Features
+-   Refer to the CODE [Security Features](../../../../core/docs/CODE%20Documentation.md#security-features) documentation for details
+
+## Design Strategy
+-   Refer to the [CODE Design Strategy](../../../../core/docs/CODE%20Documentation.md#design-strategy) documentation for details
+    -   Benefits:
+        -   Reduce the amount of custom code needed to deploy this CODE fork
--- projects/EM/hooks/post_container_hook.sh
+++ projects/EM/hooks/post_container_hook.sh
@@ -1,39 +1,89 @@
 #!/bin/bash
 
-echo "This is the EM post container hook"
 
-DB_CONN="sys/YourPassword@//db-host:1521/ORCLPDB as sysdba"
-RETRY_INTERVAL=5
+function code_container_check_ords_ready() {
+	local sys_credentials="${1}"
 
-function check_ords_schema() {
-    sqlplus -s "${DB_CONN}" <<EOF
-    SET HEAD OFF FEEDBACK OFF PAGESIZE 0 LINESIZE 1000;
-    SELECT COUNT(*) FROM dba_users WHERE username = 'ORDS_METADATA';
+	# validate the bash variable values
+	if ! cds_shared_validate_required_vars	"sys_credentials"; then
+		echo "Error: ${FUNCNAME[0]}() function required bash variable validation failed" >&2
+		return 1
+	fi
+
+    # query the ORDS package to check if it's available to ensure ORDS commands can be run
+    sqlplus -s "${sys_credentials}" <<EOF > /dev/null 2>&1
+    WHENEVER SQLERROR EXIT 1;
+    BEGIN
+    -- IF FALSE ensures code is compiled/checked for permissions, but never executed
+    EXECUTE IMMEDIATE 'BEGIN IF FALSE THEN ORDS.ENABLE_SCHEMA(p_enabled => FALSE); END IF; END;';
+    END;
+    /
     EXIT;
 EOF
+
+    # Returns 0 if sqlplus exited successfully, or 1 if ORDS.ENABLE_SCHEMA couldn't be resolved
+    return $?
 }
 
-# wait for ORDS to be installed on the DB instance
 
-# Loop until SQL*Plus returns '1' for the schema count
-until [ "$(check_ords_schema | tr -d '[:space:]')" -eq "1" ]; do
-    echo "ORDS schema not found yet. Retrying in ${RETRY_INTERVAL}s..."
-    sleep "${RETRY_INTERVAL}"
-done
+function main()
+{
+    # reference the deploy_database_scripts_func_args variable so the values can be used
+    local -n arg_ref="deploy_database_scripts_func_args"
 
-echo "ORDS schema is installed, run the custom EM database installation scripts"
+	# store the oracle admin password in a local variable
+	local sys_password="$(cat ${arg_ref[oracle_pwd_file]})"
 
-# define the database scripts map variable
-DB_SCRIPTS_MAP=()
+	# define the SYS credentials for use in deployment scripts based on environment variables:
+	local sys_credentials="SYS/\"${sys_password}\"@${arg_ref[dbhost]}:${arg_ref[dbport]}/${arg_ref[dbservicename]} AS SYSDBA"
 
-# define the database scripts mapping using the pipe character as a delimiter
-# The elements should contain encoded values with the "|" character as the delimiter: sql path (within container)|sql script file|User Secret Name|Password Secret Name|Script Password Secrets (this can be one or more optional pipe-delimited secret names when a password is injected into the script - examples include a CREATE USER command) 
-DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/EM/modules/em-database/db|@dev_container_setup/create_docker_schemas.sql|oracle_admin_user|oracle_pwd|em_db_password_secret|em_app_password_secret")
-DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/EM/modules/em-database/db|@automated_deployments/deploy_dev_container.sql|em_db_username_secret|em_db_password_secret")
-DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/EM/modules/em-database/db|@automated_deployments/deploy_dev_app_container.sql|em_app_username_secret|em_app_password_secret")
+    # Grant privilege so SYS can run ORDS package routines
+    sqlplus -s "${sys_credentials}" <<EOF > /dev/null 2>&1
+    WHENEVER SQLERROR CONTINUE;
+    GRANT INHERIT PRIVILEGES ON USER SYS TO ORDS_METADATA;
+    EXIT;
+EOF
 
+    # check if the $APP_SCHEMA_NAME exists in the database, if so then do not run the deployment scripts
+    if code_container_check_database_initialized "${sys_credentials}" "${APP_SCHEMA_NAME}"; then
+        # $APP_SCHEMA_NAME exists in the database, there is no need to run the database deployment scripts
 
-echo "execute the EM deployment scripts"
+        echo "${APP_SCHEMA_NAME} exists in the database, do not re-deploy the database and objects"
 
-# execute the SYS, EM, EM_ORDS_APP schema scripts
-code_container_deploy_custom_database_scripts "deploy_database_scripts_func_args" "DB_SCRIPTS_MAP"
+    else
+        # $APP_SCHEMA_NAME does not exist in the database, run the deployment scripts
+        echo "${APP_SCHEMA_NAME} does not exist in the database, run the deployment scripts"
+
+        # define how many seconds to wait between each retry
+        local retry_interval=5
+
+        # wait for ORDS to be installed on the DB instance
+        echo "wait for ORDS to be installed on the DB instance"
+
+        # Loop until SQL*Plus returns '1' for the schema count, waiting retry_interval between each attempt
+        until code_container_check_ords_ready "${sys_credentials}"; do
+            echo "ORDS has not been installed yet. Retrying in ${retry_interval}s..."
+            sleep "${retry_interval}"
+        done
+
+        echo "ORDS has been installed, run the custom EM database installation scripts"
+
+        # define the database scripts map variable
+        local -a DB_SCRIPTS_MAP=()
+
+        # define the database scripts mapping using the pipe character as a delimiter
+        # The elements should contain encoded values with the "|" character as the delimiter: sql path (within container)|sql script file|User Secret Name|Password Secret Name|Script Password Secrets (this can be one or more optional pipe-delimited secret names when a password is injected into the script - examples include a CREATE USER command) 
+        DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/EM/modules/em-database/db|@dev_container_setup/create_docker_schemas.sql|oracle_admin_user|oracle_pwd|em_db_password_secret|em_app_password_secret")
+        DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/EM/modules/em-database/db|@automated_deployments/deploy_dev_container.sql|em_db_username_secret|em_db_password_secret")
+        DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/EM/modules/em-database/db|@automated_deployments/deploy_dev_ords_container.sql|em_app_username_secret|em_app_password_secret")
+
+        echo "execute the EM deployment scripts"
+
+        # execute the SYS, EM, EM_ORDS_APP schema scripts
+        code_container_deploy_custom_database_scripts "deploy_database_scripts_func_args" "DB_SCRIPTS_MAP"
+    fi
+}
+
+# run the main function
+main
+
